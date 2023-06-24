c1aba6a0ff96c5bf7398d4c8698e698ddb158953
Louis THEVENET <louis.tvnt@gmail.com>
2023-06-24 20:27:15 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SixLabors.ImageSharp;
+
+    namespace RayTracer {
+    class Program
+    {
+        static void Main(string[] args){
+            
+
+        }
+
+
+
+    }
+}

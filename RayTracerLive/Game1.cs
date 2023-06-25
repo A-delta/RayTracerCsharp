f956e9d28a35449be6a1d2bf53ef4fe0e84643f8
Louis THEVENET <louis.tvnt@gmail.com>
2023-06-25 18:18:13 +0200

    Moving sphere

--- RayTracerLive/Game1.cs
+++ RayTracerLive/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Numerics;
+using System.Diagnostics;
 
 namespace RayTracerLive;
 
@@ -8,7 +10,14 @@ public class Game1 : Game
 {
     GraphicsDeviceManager graphics;
     SpriteBatch spriteBatch;
-    Texture2D texture;
+
+    Texture2D image;
+    int height;
+    int width;
+    List<ObjectInterface> objects;
+    List<LightSource> lightSources;
+    Renderer rd;
+    System.IO.Stream stream;
 
     public Game1()
     {
@@ -18,13 +27,46 @@ public class Game1 : Game
 
     protected override void Initialize()
     {
+        height = 600;
+        width = 1000;
+
+        graphics.PreferredBackBufferWidth = width; // set this value to the desired width of your window
+        graphics.PreferredBackBufferHeight = height; // set this value to the desired height of your window
+        graphics.ApplyChanges();
+
+        objects = new List<ObjectInterface>();
+        objects.Add(
+            new Sphere(
+                radius: 15,
+                new System.Numerics.Vector3(x: 0, y: 50, -200),
+                SixLabors.ImageSharp.Color.Yellow
+            )
+        );
+
+        lightSources = new List<LightSource>();
+        lightSources.Add(new LightSource(new System.Numerics.Vector3(0, 50, 0), 1f));
+
+        rd = new Renderer(
+            width,
+            height,
+            new System.Numerics.Vector3(0, 0, 0),
+            new System.Numerics.Vector3(0, 0, 0)
+        );
+
+        spriteBatch = new SpriteBatch(GraphicsDevice);
+        this.IsFixedTimeStep = true;
         base.Initialize();
     }
 
     protected override void LoadContent()
     {
-        spriteBatch = new SpriteBatch(GraphicsDevice);
-        //texture = Texture2D.FromStream(graphics, stream);
+        var img = rd.Render(objects, lightSources, new Image<Rgb24>(width, height));
+        stream = new System.IO.MemoryStream();
+        img.Save(stream: stream, new SixLabors.ImageSharp.Formats.Jpeg.JpegEncoder());
+
+        image = Texture2D.FromStream(graphicsDevice: graphics.GraphicsDevice, stream);
+
+        stream.Dispose();
     }
 
     protected override void UnloadContent()
@@ -37,15 +79,36 @@ public class Game1 : Game
     {
         if (Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
+
+        if (Keyboard.GetState().IsKeyDown(Keys.Down))
+        {
+            objects[0].center += new System.Numerics.Vector3(0, -1, 0);
+            LoadContent();
+        }
+        if (Keyboard.GetState().IsKeyDown(Keys.Up))
+        {
+            objects[0].center += new System.Numerics.Vector3(0, +1, 0);
+            LoadContent();
+        }
+        if (Keyboard.GetState().IsKeyDown(Keys.Left))
+        {
+            objects[0].center += new System.Numerics.Vector3(-1, 0, 0);
+            LoadContent();
+        }
+        if (Keyboard.GetState().IsKeyDown(Keys.Right))
+        {
+            objects[0].center += new System.Numerics.Vector3(1, 0, 0);
+            LoadContent();
+        }
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
+        GraphicsDevice.Clear(Microsoft.Xna.Framework.Color.CornflowerBlue);
 
         spriteBatch.Begin();
-        spriteBatch.Draw(texture, Vector2.Zero, Color.White);
+        spriteBatch.Draw(image, System.Numerics.Vector2.Zero, Microsoft.Xna.Framework.Color.White);
         spriteBatch.End();
 
         base.Draw(gameTime);

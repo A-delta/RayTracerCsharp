8b260877ee884d1299f2186b845e1521923d33af
Adelta <louis.tvnt@gmail.com>
2023-07-15 19:32:15 +0200

    added constant material values to use

--- RayTracer/Material.cs
+++ RayTracer/Material.cs
@@ -1,4 +1,5 @@
 using System.Numerics;
+using System.Text.Json;
 
 public class Material
 {
@@ -7,4 +8,39 @@ public class Material
         ambientComponent,
         diffuseComponent;
     public float shininess;
+
+    public Material(string materialName)
+    {
+        using (
+            Stream stream = File.Open("../RayTracer/materials.json", FileMode.Open, FileAccess.Read)
+        )
+        {
+            Dictionary<string, List<List<string>>> data = JsonSerializer.Deserialize<
+                Dictionary<string, List<List<string>>>
+            >(stream);
+            if (data.ContainsKey(materialName))
+            {
+                var components = data[materialName];
+
+                ambientComponent.X = float.Parse(components[0][0]);
+                ambientComponent.Z = float.Parse(components[0][2]);
+                ambientComponent.Y = float.Parse(components[0][1]);
+                ambientComponent.W = 0;
+
+                diffuseComponent.X = float.Parse(components[1][0]);
+                diffuseComponent.Y = float.Parse(components[1][1]);
+                diffuseComponent.Z = float.Parse(components[1][2]);
+                diffuseComponent.W = 0;
+
+                specularComponent.X = float.Parse(components[2][0]);
+                specularComponent.Y = float.Parse(components[2][1]);
+                specularComponent.Z = float.Parse(components[2][2]);
+                specularComponent.W = 0;
+
+                shininess = float.Parse(components[3][0]);
+                return;
+            }
+            throw new Exception("This material isn't listed");
+        }
+    }
 }
--- RayTracerConsole/Program.cs
+++ RayTracerConsole/Program.cs
@@ -7,23 +7,24 @@ namespace RayTracer
     {
         static void Main(string[] args)
         {
-            int height = 1080;
-            int width = 1920;
+            int height = 800;
+            int width = height * 16 / 9;
 
-            Material emerald = new Material()
+            List<IObject> objects = new List<IObject>()
             {
-                ambientComponent = new Vector4(.0215f, .1745f, .0215f, 0),
-                diffuseComponent = new Vector4(0.07568f, 0.61424f, 0.07568f, 0),
-                specularComponent = new Vector4(0.633f, 0.727811f, 0.633f, 0),
-                shininess = .6f * 128
+                new Sphere(15, new Vector3(0, 0, -60), new Material("emerald")),
+                new Sphere(22, new Vector3(0, 0, 0), new Material("chrome")),
+                new Sphere(28, new Vector3(0, 0, 0), new Material("blackrubber"))
             };
-
-            Renderer rd = new Renderer(width, height, new Vector3(0, 0, 0), new Vector3(0, 0, 0));
-            List<IObject> objects = new List<IObject>();
-            List<LightSource> lightSources = new List<LightSource>();
-
-            objects.Add(new Sphere(15, new Vector3(0, 0, -60), emerald));
-            lightSources.Add(
+            List<LightSource> lightSources = new List<LightSource>()
+            {
+                // new LightSource()
+                // {
+                //     position = new Vector3(0, 0, 100),
+                //     ambientComponent = new Vector4(.2f, .2f, .2f, 1f),
+                //     DiffuseComponent = new Vector4(1f, 1f, 1f, 1f),
+                //     SpecularComponent = new Vector4(1f, 1f, 1f, 1f)
+                // },
                 new LightSource()
                 {
                     position = new Vector3(0, 0, 0),
@@ -31,10 +32,54 @@ namespace RayTracer
                     DiffuseComponent = new Vector4(1f, 1f, 1f, 1f),
                     SpecularComponent = new Vector4(1f, 1f, 1f, 1f)
                 }
+            };
+
+            SimpleAnimation(objects, lightSources, width, height);
+        }
+
+        private static void SimpleAnimation(
+            List<IObject> objects,
+            List<LightSource> lightSources,
+            int width,
+            int height
+        )
+        {
+            Renderer rd = new Renderer(
+                width,
+                height,
+                new Vector3(0, 50, 200),
+                new Vector3(0, 0, 0)
             );
 
-            Image<Rgb24> img = rd.Render(objects, lightSources, new Image<Rgb24>(width, height));
-            img.SaveAsPng("output.png");
+            Image<Rgb24> img;
+            Image<Rgb24> gif = rd.Render(objects, lightSources, new Image<Rgb24>(width, height));
+            var gifMetaData = gif.Metadata.GetGifMetadata();
+            var metadata = gif.Frames.RootFrame.Metadata.GetGifMetadata();
+
+            float limit = 30f;
+            float step = .1f;
+
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            for (float i = -limit; i < limit; i += step)
+            {
+                Console.WriteLine(i);
+                //the actual animation :
+                objects[0].SetPosition(new Vector3(i, 0, limit / 2 - i));
+                objects[1].SetPosition(new Vector3(limit / 2 - i, 0, i));
+                objects[2].SetPosition(new Vector3(i, 0, i));
+
+                img = new Image<Rgb24>(width, height);
+                metadata = img.Frames.RootFrame.Metadata.GetGifMetadata();
+                metadata.FrameDelay = 4;
+                gif.Frames.AddFrame(rd.Render(objects, lightSources, img).Frames.RootFrame);
+            }
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            Console.WriteLine((2 * limit / step) / ts.TotalSeconds + " fps"); //total frames / total seconds
+
+            gif.SaveAsGif("output.gif");
         }
     }
 }

640354fb31104de7743c760a57e0460488215524
Adelta <louis.tvnt@gmail.com>
2023-07-16 17:34:03 +0200

    put animation with results in a separate temp function

--- RayTracerConsole/Program.cs
+++ RayTracerConsole/Program.cs
@@ -7,7 +7,15 @@ namespace RayTracer
     {
         static void Main(string[] args)
         {
-            int height = 1080;
+            Animation5Boules();
+        }
+
+        private static void Animation5Boules()
+        {
+            // 4.1458502345971855 fps
+            //Size:28360612K  Cpu:427%  Elapsed:489.47
+
+            int height = 3000;
             int width = height * 16 / 9;
 
             List<IObject> objects = new List<IObject>()
@@ -29,9 +37,8 @@ namespace RayTracer
                 }
             };
             Renderer rd = new Renderer(width, height, new Vector3(0, 0, 50), new Vector3(0, 0, 0));
-            rd.Render(objects, lightSources, new Image<Rgb24>(width, height))
-                .SaveAsPng("test_position_camera.png");
-            //SimpleAnimation(rd, objects, lightSources, width, height);
+            //rd.Render(objects, lightSources, new Image<Rgb24>(width, height)).SaveAsPng("test_position_camera.png");
+            SimpleAnimation(rd, objects, lightSources, width, height);
         }
 
         private static void SimpleAnimation(
@@ -57,7 +64,7 @@ namespace RayTracer
             {
                 Console.WriteLine(i);
                 //the actual animation :
-                lightSources[0].position = new Vector3(i, 10, i);
+                lightSources[0].position = new Vector3(i, 10, 0);
                 // end animation
 
                 img = new Image<Rgb24>(width, height);

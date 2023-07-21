f118c9a0200f290806863d38de686d7d4be5d37b
Adelta <louis.tvnt@gmail.com>
2023-07-21 11:24:52 +0200

    Added plane object

--- RayTracer/IObjects/Plane.cs
+++ RayTracer/IObjects/Plane.cs
@@ -9,7 +9,7 @@ public class Plane : IObject
 
     public Plane(Vector3 n, Vector3 p0, Material material)
     {
-        this.n = n;
+        this.n = Vector3.Normalize(n);
         this.p0 = p0;
         this.material = material;
     }
@@ -21,21 +21,20 @@ public class Plane : IObject
 
     public Vector3 GetNormalVector(Vector3 point)
     {
-        return point - Vector3.Dot(Vector3.Normalize(point), n) * n;
+        return n;
     }
 
     public bool RayIntersect(Vector3 origin, Vector3 direction)
     {
-        return RayIntersectPoint(origin, direction) is not null;
+        return !(Vector3.Dot(-n, direction) < .0001f);
     }
 
     public Vector3? RayIntersectPoint(Vector3 origin, Vector3 direction)
     {
-        float D = Vector3.Dot(n, direction);
-        if (D == 0)
+        if (!RayIntersect(origin, direction))
             return null;
-        Vector3 w = origin - p0;
-        return p0 + w + direction * (-Vector3.Dot(n, w) / D);
+        float t = Vector3.Dot((p0 - origin), n) / (Vector3.Dot(direction, n));
+        return origin + t * direction;
     }
 
     public void SetPosition(Vector3 newPosition)

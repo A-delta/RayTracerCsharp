45bf799299df8fa2f25a57cba066b8d233b99f4f
Adelta <louis.tvnt@gmail.com>
2023-07-22 21:46:01 +0200

    fixed triangle not rendered correctly when contained in certain planes

--- RayTracer/IObjects/Triangle.cs
+++ RayTracer/IObjects/Triangle.cs
@@ -25,8 +25,8 @@ public class Triangle : IObject
     public Vector3 GetNormalVector(Vector3 origin, Vector3 direction)
     {
         var n = Vector3.Normalize(Vector3.Cross((B - A), (C - A)));
-        //Console.WriteLine(            Vector3.Dot(direction, n) + " " + ((Vector3.Dot(direction, n) > .0001f) ? n : -n));
-        return (Vector3.Dot(direction, n) > .0001f) ? n : -n;
+
+        return (Vector3.Dot(direction, n) <= 1e-6) ? -n : n;
     }
 
     public bool RayIntersect(Vector3 origin, Vector3 direction)
@@ -37,19 +37,42 @@ public class Triangle : IObject
     public Vector3? RayIntersectPoint(Vector3 origin, Vector3 direction)
     {
         var n = GetNormalVector(origin, -direction);
-        if (Vector3.Dot(n, direction) > .0001f)
+
+        if (Vector3.Dot(n, -direction) < 1e-6)
             return null;
         float t = Vector3.Dot(((A + B + C) / 3 - origin), n) / (Vector3.Dot(direction, n));
         var p = origin + t * direction;
 
-        var s = (A.X - C.X) * (p.Y - C.Y) - (A.Y - C.Y) * (p.X - C.X);
-        var u = (B.X - A.X) * (p.Y - A.Y) - (B.Y - A.Y) * (p.X - A.X);
+        var CA = A - C;
+        var CP = p - C;
 
-        if ((s < 0) != (u < 0) && s != 0 && u != 0)
+        var AB = B - A;
+        var AP = p - A;
+
+        var test = (float aA, float bA, float aP, float bP) => aA * bP - bA * aP;
+        float u = 0;
+        float s = 0;
+        if ((Math.Abs(A.X - B.X) <= 1e-6 && Math.Abs(B.X - C.X) <= 1e-6))
+        {
+            s = test(CA.Z, CA.Y, CP.Z, CP.Y);
+            u = test(AB.Z, AB.Y, AP.Z, AP.Y);
+        }
+        else if ((A.Y - B.Y <= 1e-6 && B.Y - C.Y <= 1e-6))
+        {
+            s = test(CA.Z, CA.X, CP.Z, CP.X);
+            u = test(AB.Z, AB.X, AP.Z, AP.X);
+        }
+        else
+        {
+            s = test(CA.Y, CA.X, CP.Y, CP.X);
+            u = test(AB.Y, AB.X, AP.Y, AP.X);
+        }
+
+        if ((s <= 1e-6) ^ (u <= 1e-6))
             return null;
 
-        var d = (C.X - B.X) * (p.Y - B.Y) - (C.Y - B.Y) * (p.X - B.X);
-        return (d == 0 || (d < 0) == (s + u <= 0)) ? p : null;
+        var d = (C.Z - B.Z) * (p.Y - B.Y) - (C.Y - B.Y) * (p.Z - B.Z);
+        return (!(d <= 1e-6 ^ (s + u <= 1e-6))) ? p : null;
     }
 
     public void SetPosition(Vector3 newPosition)

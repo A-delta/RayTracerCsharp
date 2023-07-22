182974faa248460f68af26fdf7f7a592f41f67f9
Adelta <louis.tvnt@gmail.com>
2023-07-22 22:54:15 +0200

    displays rabbit file

--- RayTracerConsole/Program.cs
+++ RayTracerConsole/Program.cs
@@ -7,39 +7,17 @@ namespace RayTracer
     {
         static void Main(string[] args)
         {
-            int height = 1920 * 2;
+            int height = 1080;
             int width = height * 16 / 9;
-            Renderer rd = new Renderer(
-                width,
-                height,
-                new Vector3(14, 2.5f, 14),
-                new Vector3(0, 0, 0)
-            );
+            Renderer rd = new Renderer(width, height, new Vector3(0, 0, 300), new Vector3(0, 0, 0));
 
-            //var objects = STLReader.ReadFile("blank-elephant/body_ascii.stl");
-            var objects = new List<IObject>()
-            {
-                new Sphere(.1f, new Vector3(5 + 7.55862f, 0, 4.1395f), new Material("emerald")),
-                new Sphere(.1f, new Vector3(5 + 7.55862f, 0, 4.47308f), new Material("copper")),
-                new Sphere(.1f, new Vector3(5 + 7.55862f, 5, 4.30629f), new Material("brass")),
-                new Triangle(
-                    new Vector3(5 + 7.55862f, 0, 4.1395f),
-                    new Vector3(5 + 7.55862f, 0, 4.47308f),
-                    new Vector3(5 + 7.55862f, 5, 4.30629f),
-                    new Material("ruby")
-                ),
-                new Triangle(
-                    new Vector3(14, 0, 2),
-                    new Vector3(15, 2.5f, 2),
-                    new Vector3(16, 1, 0),
-                    new Material("ruby")
-                )
-            };
+            var objects = STLReader.ReadFile("lapin_ascii.stl");
+
             var lights = new List<LightSource>();
             lights.Add(
                 new LightSource()
                 {
-                    position = new Vector3(21, 0, 0),
+                    position = rd.position + new Vector3(0, 0, -10),
                     ambientComponent = new Vector4(.2f, .2f, .2f, .2f),
                     DiffuseComponent = new Vector4(1f, 1f, 1f, 1f),
                     SpecularComponent = new Vector4(1f, 1f, 1f, 1f)
@@ -47,10 +25,6 @@ namespace RayTracer
             );
             //AddRectangularLight(lights, .8f, new Vector3(-20, 30, 0), new Vector3(20, 30, 0));
 
-            foreach (var t in objects)
-            {
-                t.PrintInformation();
-            }
 
             var img = rd.Render(objects, lights, new Image<Rgb24>(width, height));
             img.SaveAsPng("output.png");

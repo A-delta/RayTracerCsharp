4e989637229e6b6797f5f1020c485b3a0b1f3a4d
Adelta <louis.tvnt@gmail.com>
2023-07-30 11:12:35 +0200

    Added some error handling

--- RayTracer/Material.cs
+++ RayTracer/Material.cs
@@ -10,41 +10,47 @@ public class Material
         albedo;
     public float shininess;
 
-    public Material(string materialName)
+    public Material(string materialName, string materialsPath)
     {
-        using (
-            Stream stream = File.Open("../RayTracer/materials.json", FileMode.Open, FileAccess.Read)
-        )
+        try
         {
-            var data = JsonSerializer.Deserialize<Dictionary<string, List<List<string>>>>(stream);
-            if (data.ContainsKey(materialName))
+            using (Stream stream = File.Open(materialsPath, FileMode.Open, FileAccess.Read))
             {
-                var components = data[materialName];
+                var data = JsonSerializer.Deserialize<Dictionary<string, List<List<string>>>>(stream);
+                if (data.ContainsKey(materialName))
+                {
+                    var components = data[materialName];
 
-                ambientComponent.X = float.Parse(components[0][0]);
-                ambientComponent.Z = float.Parse(components[0][2]);
-                ambientComponent.Y = float.Parse(components[0][1]);
-                ambientComponent.W = 0;
+                    ambientComponent.X = float.Parse(components[0][0]);
+                    ambientComponent.Z = float.Parse(components[0][2]);
+                    ambientComponent.Y = float.Parse(components[0][1]);
+                    ambientComponent.W = 0;
 
-                specularComponent.X = float.Parse(components[1][0]);
-                specularComponent.Y = float.Parse(components[1][1]);
-                specularComponent.Z = float.Parse(components[1][2]);
-                specularComponent.W = 0;
+                    specularComponent.X = float.Parse(components[1][0]);
+                    specularComponent.Y = float.Parse(components[1][1]);
+                    specularComponent.Z = float.Parse(components[1][2]);
+                    specularComponent.W = 0;
 
-                diffuseComponent.X = float.Parse(components[2][0]);
-                diffuseComponent.Y = float.Parse(components[2][1]);
-                diffuseComponent.Z = float.Parse(components[2][2]);
-                diffuseComponent.W = 0;
+                    diffuseComponent.X = float.Parse(components[2][0]);
+                    diffuseComponent.Y = float.Parse(components[2][1]);
+                    diffuseComponent.Z = float.Parse(components[2][2]);
+                    diffuseComponent.W = 0;
 
-                albedo.X = float.Parse(components[3][0]); // reflec
-                albedo.Y = float.Parse(components[3][1]); // diff
-                albedo.Z = float.Parse(components[3][2]); // spec
-                albedo.W = 0; // refrac
+                    albedo.X = float.Parse(components[3][0]); // reflec
+                    albedo.Y = float.Parse(components[3][1]); // diff
+                    albedo.Z = float.Parse(components[3][2]); // spec
+                    albedo.W = 0; // refrac
 
-                shininess = 128 * float.Parse(components[4][0]);
-                return;
+                    shininess = 128 * float.Parse(components[4][0]);
+                    return;
+                }
+                Console.Error.WriteLine("This material isn't listed in the provided file");
             }
-            throw new Exception("This material isn't listed");
+        }
+        catch (DirectoryNotFoundException)
+        {
+            Console.Error.WriteLine("Materials file not found");
+            Environment.Exit(20);
         }
     }
 }
